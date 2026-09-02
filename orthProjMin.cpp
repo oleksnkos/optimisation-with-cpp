f7b81d788d75281e14596de6b0b4febb233c9686
oleksnkos <oleksandrparpula@gmail.com>
2026-09-02 19:33:45 +0300

    Add inner product and induced norm for functions

--- orthProjMin.cpp
+++ orthProjMin.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <functional>
 
-//Implementing integration via Simpson's rule as error is O(h^4)
-double simpsonIntegr(double (*f)(double), double a, double b, int n){
+//Function product
+std::function<double(double)> product(double (*f)(double), double (*g)(double)){
+    return [f, g](double x){
+        return f(x) * g(x);
+    };
+}
+
+//Integration via Simpson's rule as the error is O(h^4)
+double simpsonIntegr(std::function<double(double)> f, double a, double b, int n){
     
-    if(n % 2 == 1) n++;
+    if(n % 2 == 1) n++; //Number of partitions has to be even
 
     double h = (b - a) / n;
     double sum = f(a) + f(b);
@@ -21,14 +29,34 @@ double simpsonIntegr(double (*f)(double), double a, double b, int n){
     return sum;
 }
 
-double f(double x){
-    return sin(x) + pow(x, 2);
+//Function inner product
+double integrInnerProd(double (*f)(double), double (*g)(double), double a=-1, double b=1, int n = 100000){
+    return simpsonIntegr(product(f, g), a, b, n);
 }
+
+//Induced norm
+double inducedNorm(double (*f)(double), double a=-1, double b=1, int n=100000){
+    return std::sqrt(integrInnerProd(f, f, a, b, n));
+}
+
+
+double f(double x){
+    return sin(x);
+}
+
+double g(double x){
+    return pow(x, 2);
+}
+
+
 
 int main(){
 
-    double approxIntegr = simpsonIntegr(f, 2, 5, 1000);
-    std::cout << approxIntegr;
+    double in = integrInnerProd(f, g, 2, 5);
+    std::cout << in << ", " << simpsonIntegr(product(f, g), 2, 5, 100000) <<'\n';
+
+    double indn = inducedNorm(f, 2, 5);
+    std::cout << indn << ", " << std::sqrt(integrInnerProd(f, f, 2, 5)) <<'\n';
 
     return 0;
 }
